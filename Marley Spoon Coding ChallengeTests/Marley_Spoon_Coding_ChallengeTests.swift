5666b8a9a519de12ae5827aa8755bb953e8fed9e
Rachel Fong <rach_pohyee13@hotmail.com>
2020-02-22 22:41:37 +0800

    Initial Commit

---
+++ Marley Spoon Coding ChallengeTests/Marley_Spoon_Coding_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Marley_Spoon_Coding_ChallengeTests.swift
+//  Marley Spoon Coding ChallengeTests
+//
+//  Created by pohyee on 22/02/2020.
+//  Copyright © 2020 pohyee. All rights reserved.
+//
+
+import XCTest
+@testable import Marley_Spoon_Coding_Challenge
+
+class Marley_Spoon_Coding_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
