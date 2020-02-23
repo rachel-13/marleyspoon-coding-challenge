c6d7f9efc2ff8f51c85e1c087a7b6c06505b4576
Rachel Fong <rach_pohyee13@hotmail.com>
2020-02-24 00:21:44 +0800

    Rearrange details view

--- Marley Spoon Coding Challenge/View/RecipeDetailView.swift
+++ Marley Spoon Coding Challenge/View/RecipeDetailView.swift
@@ -13,52 +13,58 @@ struct RecipeDetailView: View {
   let viewModel: RecipeDetailViewModel
   var body: some View {
     
-    VStack {
-      ImageView(withURL: viewModel.photoUrl, maxWidth: UIScreen.main.bounds.width, maxHeight: 300)
-      
-      Text(viewModel.title)
-        .fontWeight(Font.Weight.semibold)
-        .font(Font.system(size: 20))
-        .padding()
-      
-      Text(viewModel.description)
-        .fontWeight(Font.Weight.regular)
-        .font(Font.system(size: 15))
-        .padding()
-      
-      if !viewModel.chefName.isEmpty {
-        Text("Created By: \(viewModel.chefName)")
-          .fontWeight(Font.Weight.light)
-          .font(Font.system(size: 14))
+    VStack(alignment: .leading) {
+      VStack(alignment: .center, spacing: 0) {
+        ImageView(withURL: viewModel.photoUrl, maxWidth: UIScreen.main.bounds.width, maxHeight: 300)
+        
+        Text(viewModel.title)
+          .fontWeight(Font.Weight.semibold)
+          .font(Font.system(size: 20))
+          .padding()
+        
+        Text(viewModel.description)
+          .fontWeight(Font.Weight.regular)
+          .font(Font.system(size: 15))
           .padding()
       }
       
-      if viewModel.calories > 0 {
-        Text("Calories: \(viewModel.calories)")
-          .fontWeight(Font.Weight.light)
-          .font(Font.system(size: 14))
-          .padding()
-      }
-      
-      HStack {
-        if !viewModel.recipeTags.isEmpty {
-          Text("Tags: ")
-            .fontWeight(Font.Weight.regular)
+      VStack(alignment: .leading, spacing: 0) {
+        if !viewModel.chefName.isEmpty {
+          Text("Created By: \(viewModel.chefName)")
+            .fontWeight(Font.Weight.light)
             .font(Font.system(size: 14))
-            .padding()
-          
-          ForEach(viewModel.recipeTags, id: \.self) { tagName in
-            Text("\(tagName)")
-              .fontWeight(Font.Weight.light)
-              .font(Font.system(size: 14))
-              .padding()
+            .padding(.leading)
+        }
+        
+        if viewModel.calories > 0 {
+          Text("Calories: \(viewModel.calories)")
+            .fontWeight(Font.Weight.light)
+            .font(Font.system(size: 14))
+            .padding([.leading, .top, .bottom])
+        }
+        
+        if !viewModel.recipeTags.isEmpty {
+          HStack(alignment: .lastTextBaseline, spacing: 0) {
+              Text("Tags: ")
+                .fontWeight(Font.Weight.regular)
+                .font(Font.system(size: 14))
+                .padding(.leading)
+              
+              ForEach(viewModel.recipeTags, id: \.self) { tagName in
+                Button(action: {
+                  // do nothing
+                }) {
+                  Text("\(tagName) ")
+                                   .fontWeight(Font.Weight.light)
+                                   .font(Font.system(size: 14))
+                }
+              }
           }
         }
       }
       
       Spacer()
-    }
-    
+    }.offset(x: 0, y: -50)
   }
   
   init(recipeDetailViewModel: RecipeDetailViewModel) {
